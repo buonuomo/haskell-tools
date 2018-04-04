31f351643b2f080573729720e5fe9fb921d03bea
Noah Goodman <ngoodman@uchicago.edu>
2018-04-04 14:18:46 -0500

    Added a bunch of comments

--- archive.hs
+++ archive.hs
@@ -1,3 +1,13 @@
+{- A program a that creates a folder or folders for a given id string (eg. 16),
+    and moves all files containing that id string into the folder 
+
+    Eg. >>> ./archive 3
+        creates directory 'day-3' if it doesnt already exist, and moves all
+        files in current directory containing '3' in their name to day-3.
+
+   Author: Noah Goodman, 2017
+-}
+
 module Main where
 
 import System.Directory
@@ -5,19 +15,30 @@ import System.Environment (getArgs)
 import Control.Monad (guard,filterM)
 import Data.List (subsequences)
 
+-- Function that turns id string into name of directory
 dayDir :: String -> FilePath
 dayDir day = "day-" ++ day
 
+-- Creates a given directory if not already there
 makeDayDir :: String -> IO ()
 makeDayDir day = createDirectoryIfMissing True (dayDir day)
 
+-- Test to see if a filename contains the id string
 containsDay :: String -> String -> Bool
 containsDay day file = day `elem` subsequences file 
 
 main :: IO ()
 main = do
     args <- getArgs
-    files <- filterM doesFileExist =<< listDirectory "."
-    let archives = [(day,file) | day <- args, file <- files, containsDay day file]
-    mapM makeDayDir args
-    mapM_ (\(day,file) -> renamePath file (dayDir day ++ "\\" ++ file)) archives
+    case args of 
+      [] -> putStrLn "Usage: archive [id_strings]"
+      _  -> do
+          -- List of all non-directory files
+          files <- filterM doesFileExist =<< listDirectory "."
+          -- list of (key,value) pairs where key == id string, and value == list of
+          -- files in current directory that contain that id string
+          let archives = [(day,file) | day <- args, file <- files, containsDay day file]
+          -- Actually make the directories and move the files to those directories, as
+          -- specified by archives
+          mapM makeDayDir args
+          mapM_ (\(day,file) -> renamePath file (dayDir day ++ "\\" ++ file)) archives
--- txt.hs
+++ txt.hs
@@ -1,15 +1,29 @@
+{- Simple program that copies all .hs files in current directory to .txt files
+    of the same name. 
+
+   I originally used this for a class which required printing out haskell source
+    for handing in homework, since the printer could not print .hs files
+
+   Author: Noah Goodman, 2017
+-}
+
 module Main where
 
 import System.Directory
 import Control.Monad (guard)
 
+-- Checks if a file has the .hs extension
 isHask :: FilePath -> Bool
 isHask = (=="sh.") . take 3 . reverse
 
+-- replaces .hs extension (or any two letter extension) with .txt
 replaceHs :: FilePath -> FilePath
-replaceHs = reverse . ("txt." ++) . drop 3 . reverse
+replaceHs = reverse . ("txt" ++) . drop 2 . reverse
 
+-- Execution of the copying procedure in the current directory
 main = do 
     ls <- listDirectory "."
+    -- makes (key,value) pairs for files in current directory, where key == .hs
+    -- file, and value == .txt file with same name
     let txts = [(orig,txt) | orig <- ls, isHask orig, let txt = replaceHs orig]
     mapM_ (uncurry copyFile) txts   
